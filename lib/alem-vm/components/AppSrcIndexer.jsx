06f4c0329150f73556e624d44972e1414423d198
Wenderson Pires <wendersonpdas@gmail.com>
2024-06-03 17:41:07 -0300

    add AppSrcIndexer to be used when ejectStatefulComponents is true

--- lib/alem-vm/components/AppSrcIndexer.jsx
+++ lib/alem-vm/components/AppSrcIndexer.jsx
@@ -1,17 +1,13 @@
 /**
- * This file uses the App's code to render the App widget
+ * This file uses Widget's src to get the App Widget.
+ *
+ * It's used when the alem config "ejectStatefulComponents" is true
  */
 const AlemApp = useMemo(() => {
   if (!props.alem.ready) {
     return "";
   }
 
-  // Modules, Codes Wrapper, Recursos que dependem das propriedades globais devem ser colocadas em segunda camada, senão props retorna
-  // sem os dados do além.
-  // Todas as propriedades que dependem de algum recurso do Além deve ser colocado nessa segunda camada, por exemplo:
-  // "props.alem.isDevelopment", "props.alem.getAlemEvironment()", etc
-  // TODO: colocar isso em um arquivo?
-  // TODO: 2 - colocar condicional para: se usar modulos, faz esse processo de usar o layer2, senao, carrega diretamente o APP como antes
   const widgetLayer2code = `
   const props = {
     ...props,
@@ -26,9 +22,11 @@ const AlemApp = useMemo(() => {
   };
 
   return (
-    <Widget loading=" " code={props.alem.componentsCode.App} props={props} />
+    <Widget loading=" " src={"APP_INDEXER_WIDGET_SRC"} props={props} />
   )
   `;
+
+  // staging.potlock.near/widget/potlock.ConfigForm
 
   return (
     <AlemTheme>
